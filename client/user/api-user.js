fb182046b521c3c1b6becb014c99dcf3e28c4def
C. Gremaud <colyn.gremaud@gmail.com>
2021-06-21 11:38:29 -0500

    finished api-user.js

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -28,5 +28,54 @@ const list = async (signal) => {
     } catch (err) {
         console.log(err)
     }
+}
+//not sure what this signal param is doing. Shows up in a few places
+const read = async (params, credentials, signal) => {
+    try {
+        let response = await fetch('/api/users' + params.userId, {
+            method: 'GET',
+            signal: signal,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            }
+        }) 
+        return await response.json()
+    } catch (err) {
+        console.log(err)
+    }
+}
 
+const update = async (params, credentials, user) => {
+    try {
+        let response = await fetch ('api/users/' + params.userId, {
+            method: 'PUT',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            },
+            body: JSON.stringify(user)
+        })
+        return await response.json()
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+const remove = async(params, credentials) => {
+    try {
+        let resposne = await fetch('/api/users/' + params.userId, {
+            method: 'DELETE',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + credentials.t
+            }
+        })
+        return await response.json()
+    } catch(err) {
+
+    }
 }
