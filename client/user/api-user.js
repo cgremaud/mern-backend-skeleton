d3dbbc634ac7b5cdf0dca399f4312198fc225852
David Hackett <davidh@mediacross.com>
2021-07-04 17:51:29 -0500

    some temp fixes

--- client/user/api-user.js
+++ client/user/api-user.js
@@ -1,83 +1,83 @@
 //this file contains methods for accessing each of the user CRUD API endpoints
 
 //creates new user. Returns a promise.json()
-const create = async(user) => {
-    try {
-        let response = await fetch('/api/users', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            },
-            //this line converts the JS user obj into a JSON formatted string
-            body: JSON.stringify(user)
-        })
-        return await response.json()
-    } catch (err) {
-        console.log(err)
-    }
-}
+const create = async (user) => {
+  try {
+    let response = await fetch("/api/users", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      //this line converts the JS user obj into a JSON formatted string
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
 //returns an array
 const list = async (signal) => {
-    try {
-        let response = await fetch('/api/users', {
-            method: 'GET',
-            signal: signal,
-        })
-        return await response.json()
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    let response = await fetch("/api/users", {
+      method: "GET",
+      signal: signal,
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
 //not sure what this signal param is doing. Shows up in a few places
 const read = async (params, credentials, signal) => {
-    try {
-        let response = await fetch('/api/users' + params.userId, {
-            method: 'GET',
-            signal: signal,
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + credentials.t
-            }
-        }) 
-        return await response.json()
-    } catch (err) {
-        console.log(err)
-    }
-}
+  try {
+    let response = await fetch("/api/users/" + params.userId, {
+      method: "GET",
+      signal: signal,
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 const update = async (params, credentials, user) => {
-    try {
-        let response = await fetch ('api/users/' + params.userId, {
-            method: 'PUT',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + credentials.t
-            },
-            body: JSON.stringify(user)
-        })
-        return await response.json()
-    } catch(err) {
-        console.log(err)
-    }
-}
+  try {
+    let response = await fetch("api/users/" + params.userId, {
+      method: "PUT",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+      body: JSON.stringify(user),
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-const remove = async(params, credentials) => {
-    try {
-        let resposne = await fetch('/api/users/' + params.userId, {
-            method: 'DELETE',
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + credentials.t
-            }
-        })
-        return await response.json()
-    } catch(err) {
-        console.log(err)
-    }
-}
+const remove = async (params, credentials) => {
+  try {
+    let resposne = await fetch("/api/users/" + params.userId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-export { create, list, read, update, remove }
+export { create, list, read, update, remove };
