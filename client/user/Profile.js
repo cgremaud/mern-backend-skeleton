675a089a4dd6b96ab1e7fa10bd1ece747ea06e6c
C. Gremaud <colyn.gremaud@gmail.com>
2021-07-04 08:57:35 -0500

    no clue why profile isn't working. throws name is undefined???

--- client/user/Profile.js
+++ client/user/Profile.js
@@ -18,74 +18,77 @@ import {read} from './api-user.js'
 import {Redirect, Link} from 'react-router-dom'
 
 const useStyles = makeStyles(theme => ({
-    root: theme.mixins.gutters({
-      maxWidth: 600,
-      margin: 'auto',
-      padding: theme.spacing(3),
-      marginTop: theme.spacing(5)
-    }),
-    title: {
-      marginTop: theme.spacing(3),
-      color: theme.palette.protectedTitle
-    }
-  }))
+  root: theme.mixins.gutters({
+    maxWidth: 600,
+    margin: 'auto',
+    padding: theme.spacing(3),
+    marginTop: theme.spacing(5)
+  }),
+  title: {
+    marginTop: theme.spacing(3),
+    color: theme.palette.protectedTitle
+  }
+}))
 
-  export default function Profile({ match }) {
-      const [user, setUser] = useState([])
-      const [redirectToSignin, setRedirectToSignin] = useState(false)
+export default function Profile({ match }) {
+  const classes = useStyles()
+  const [user, setUser] = useState({})
+  const [redirectToSignin, setRedirectToSignin] = useState(false)
+  const jwt = auth.isAuthenticated()
 
-      useEffect(() => {
-          const abortController = new AbortController()
-          const signal = abortController.signal
-          const jwt = auth.isAuthenticated()
-          read({
-              userId: match.params.userId
-          }, {t: jwt.token}, signal).then((data) => {
-              if (data && data.error) {
-                  setRedirectToSignin(true)
-              } else {
-                  setUser(data)
-              }
-          })
+  useEffect(() => {
+    const abortController = new AbortController()
+    const signal = abortController.signal
 
-          return function cleanup() {
-              abortController.abort()
-          }
-      }, [match.params.userId]) //add match.params.userId as second argument insures it will only rerender when userId changes. 
-
-      if (redirectToSignin) {
-          return <Redirect to = "/signin" />
+    read({
+      userId: match.params.userId
+    }, {t: jwt.token}, signal).then((data) => {
+      if (data && data.error) {
+        setRedirectToSignin(true)
+      } else {
+        setUser(data)
       }
+    })
 
-      return (
-          <Paper className = {classes.root} elevation = {4}>
-              <Typography variant = "h6" className = {classes.title}>
-                  Profile
-              </Typography>
-              <List dense>
-                  <ListItem>
-                      <ListItemAvatar>
-                          <Avatar>
-                              <Person/>
-                          </Avatar>
-                      </ListItemAvatar>
-                      <ListItemText primary = {user.name} secondary = {user.email} />{
-                        auth.isAuthenticated().user && auth.isAuthenticated().user._id == user._id &&
-                        (<ListItemSecondaryAction>
-                            <Link to={"/user/edit/" + user._id}>
-                            <IconButton aria-label="Edit" color="primary">
-                                <Edit/>
-                            </IconButton>
-                            </Link>
-                            <DeleteUser userId={user._id}/>
-                        </ListItemSecondaryAction>)
-                        }
-                  </ListItem>
-                  <Divider />
-                  <ListItem>
-                      <ListItemText primary = { "Joined: " + (new Date(user.created)).toDateString()}/>
-                  </ListItem>
-              </List>
-          </Paper>
-      )
+    return function cleanup(){
+      abortController.abort()
+    }
+
+  }, [match.params.userId])
+  
+    if (redirectToSignin) {
+      return <Redirect to='/signin'/>
+    }
+    return (
+      <Paper className={classes.root} elevation={4}>
+        <Typography variant="h6" className={classes.title}>
+          Profile
+        </Typography>
+        <List dense>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <Person/>
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={user.name} secondary={user.email}/> {
+             auth.isAuthenticated().user && auth.isAuthenticated().user._id == user._id &&
+              (<ListItemSecondaryAction>
+                <Link to={"/user/edit/" + user._id}>
+                  <IconButton aria-label="Edit" color="primary">
+                    <Edit/>
+                  </IconButton>
+                </Link>
+                <DeleteUser userId={user._id}/>
+              </ListItemSecondaryAction>)
+            }
+          </ListItem>
+          <Divider/>
+          <ListItem>
+            <ListItemText primary={"Joined: " + (
+              new Date(user.created)).toDateString()}/>
+          </ListItem>
+        </List>
+      </Paper>
+    )
   }
